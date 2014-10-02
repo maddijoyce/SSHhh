fa64363c098b4bf1896e5f28072b4ef9ab719f12
Maddison Joyce <maddi@maddijoyce.com>
2014-10-02 11:23:55 +1000

    Initial Commit

---
+++ SSHhh/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SSHhh
+//
+//  Created by Maddison Joyce on 2/10/2014.
+//  Copyright (c) 2014 Maddison Joyce. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SSHhh/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SSHhh
+//
+//  Created by Maddison Joyce on 2/10/2014.
+//  Copyright (c) 2014 Maddison Joyce. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
